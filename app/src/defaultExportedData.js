15ce95cf989ea5771c13b2099be48eafa32edc0d
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-20 17:37:19 +0000

    Add Export Examples

---
+++ app/src/defaultExportedData.js
@@ -0,0 +1,15 @@
+// We can only have one default export per module
+// export default [
+// 	{
+// 		name: 'Aaron',
+// 		age: 32
+// 	},
+// 	{
+// 		name: 'Kate',
+// 		age: 23
+// 	},
+// 	{
+// 		name: 'Erika',
+// 		age: 29
+// 	}
+// ];
---
+++ app/src/namedExportData.js
@@ -0,0 +1,28 @@
+// export const namedPeopleData = [
+//   {
+//     name: "Aaron",
+//     age: 32
+//   },
+//   {
+//     name: "Kate",
+//     age: 23
+//   },
+//   {
+//     name: "Erika",
+//     age: 29
+//   },
+// ]
+
+// We can have multiple named exports in a file
+// export function increaseAge(person) {
+//   return {
+//     ...person,
+//     age: person.age + 1
+//   }
+// }
+
+// We can also have one default export in the same file as named exports
+// export default {
+//   increaseAge,
+//   namedPeopleData
+// }
