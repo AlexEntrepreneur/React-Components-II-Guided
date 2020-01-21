bff0912c6ffc8276be3b66b3671a6d51cdf8af18
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-21 11:11:39 +0000

    .

--- app/src/components/StudentCard.js
+++ app/src/components/StudentCard.js
@@ -3,10 +3,26 @@ import React from "react";
 export default function StudentCard(props) {
 
   return (
-  <div className="student-card" >
-      <h1>title</h1>
-      <h2>body</h2>
-      <img src={props.student.img} alt="image"/>
+    <div className="student-card">
+      <img src={props.student.img} alt={props.student.name} />
+      <div className="student-info">
+        <h3>
+          <strong>Name: </strong>
+          {props.student.name}
+        </h3>
+        <p>
+          <strong>Age: </strong>
+          {props.student.age}
+        </p>
+        <p>
+          <strong>Identity: </strong>
+          {props.student.identity}
+        </p>
+        <p>
+          <strong>Best Friends: </strong>
+          {props.student.bestFriend}
+        </p>
+      </div>
   </div>
   )
 }
